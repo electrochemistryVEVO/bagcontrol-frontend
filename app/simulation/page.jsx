8ee18c883df5b69a66e7958a682af284922f2025
electrochemistryVEVO <edwardxvi09@gmail.com>
2026-05-08 12:08:53 -0500

    maplibre integrado

--- app/simulation/page.jsx
+++ app/simulation/page.jsx
@@ -1,3 +1,15 @@
+'use client'
+import {Map} from '@vis.gl/react-maplibre';
+
 export default function Home(){
-    return (<div>Pagina de simulacion</div>);
+    return (<div style={{height: "80vh"}}>Pagina de simulación
+        <Map
+            initialViewState={{
+                longitude: 0,
+                latitude: 0,
+                zoom: 3.5
+            }}
+            mapStyle="https://demotiles.maplibre.org/style.json"
+        />
+    </div>);
 }
