34c83eb747360226f41f8cd33cbb8ceff4d0166b
electrochemistryVEVO <edwardxvi09@gmail.com>
2026-05-11 09:41:35 -0500

    cantidad de maletas en popup

--- app/shared/models/Aeropuerto.ts
+++ app/shared/models/Aeropuerto.ts
@@ -5,6 +5,7 @@ export class Aeropuerto{
     public continente !: string;
     public gmt !: number;
     public capacidadAlmacen !: number;
+    public cantidadAlmacen : number = 0;
     public latitud !: number;
     public longitud !: number;
 
--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -1,14 +1,27 @@
 'use client'
-import {Map, Marker} from '@vis.gl/react-maplibre';
-import {useEffect, useState} from "react";
+import {
+    Map,
+    MapGeoJSONFeature,
+    MapLayerMouseEvent,
+    MapSourceDataEvent,
+    Marker,
+    Popup,
+    PopupInstance
+} from '@vis.gl/react-maplibre';
+import {useEffect, useRef, useState} from "react";
 import {Aeropuerto} from "../shared/models/Aeropuerto";
 import {ObtenerAeropuertos} from "@/app/services/simulation.service";
 import RoomIcon from '@mui/icons-material/Room';
 import {FeatureCollection} from "geojson";
+import 'maplibre-gl/dist/maplibre-gl.css';
 import {Source,Layer,CircleLayerSpecification} from "@vis.gl/react-maplibre";
+
 
 export default function Home(){
     const [airports,setAirports] = useState<Aeropuerto[]>([]);
+    const [showPopup,setShowPopup] = useState(false);
+    const [selAirport,setSelAirport] = useState<MapGeoJSONFeature|null>(null);
+    const popupRef = useRef<PopupInstance|null>(null);
     useEffect(()=>{
         ObtenerAeropuertos().then((res)=>setAirports(res))
     },[])
@@ -20,14 +33,23 @@ export default function Home(){
                 zoom: 3.5
             }}
             mapStyle="https://demotiles.maplibre.org/style.json"
+            onMouseEnter={(event : MapLayerMouseEvent)=>{
+                console.log("over")
+                setSelAirport(event.features ? event.features[0] : null)
+                setShowPopup(true)
+            }}
+            onMouseLeave={()=>{console.log("left");setShowPopup(false)}}
+            interactiveLayerIds={["point"]}
         >
-            <AirportMarkers airports={airports}/>
+            <AirportMarkers airports={airports} showPopup={showPopup} selAirport={selAirport}/>
         </Map>
     </div>);
 }
 
 type AirportMarkersProps = {
     airports : Aeropuerto[]
+    showPopup : boolean
+    selAirport : MapGeoJSONFeature|null
 }
 
 function AirportMarkers(props : AirportMarkersProps){
@@ -35,7 +57,7 @@ function AirportMarkers(props : AirportMarkersProps){
         type: "FeatureCollection",
         features: props.airports.map((airport)=>({
             type: "Feature",
-            properties: {},
+            properties: {...airport},
             geometry: {type: "Point",coordinates: [airport.latitud,airport.longitud]}
         }))
     }
@@ -49,7 +71,25 @@ function AirportMarkers(props : AirportMarkersProps){
             'circle-color': '#007cbf'
         }
     };
+    const popupRef = useRef<PopupInstance|null>(null);
+
+    useEffect(() => {
+        popupRef.current?.trackPointer();
+        console.log(popupRef.current ? "true" : "false")
+        console.log("son")
+    })
+
+    const showPopup = props.showPopup
+    const selAirport = props.selAirport
+    //const [showPopup,setShowPopup] = useState(false);
+    //const [selAirport,setSelAirport] = useState<Aeropuerto|null>(null);
     return (<>
+        {showPopup ? (
+            <Popup longitude={-100} latitude={40}
+                   anchor="bottom" ref={popupRef}>
+                <b>{selAirport?.properties.codigoIata}</b>
+                <p>{selAirport?.properties.cantidadAlmacen}/{selAirport?.properties.capacidadAlmacen} maletas</p>
+            </Popup>) : (<div></div>)}
         <Source id="my-data" type="geojson" data={geojson}>
             <Layer {...layerStyle} />
         </Source>
