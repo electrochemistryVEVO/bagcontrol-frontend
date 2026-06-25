1b1f7b5ef61e144336f1415d0551f23ec94d3384
electrochemistryVEVO <edwardxvi09@gmail.com>
2026-06-25 17:37:14 -0500

    cambios para despliegue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "3.91.199.44" },
