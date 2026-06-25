36923de92960665fb150e68ba2a543feeb1ea778
electrochemistryVEVO <edwardxvi09@gmail.com>
2026-06-25 17:29:31 -0500

    cambios para despliegue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.56.1'],
+  allowedDevOrigins: ['192.168.56.1','3.91.199.44'],
+  async headers() {
+    return [
+      {
+        source: "*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "3.91.199.44" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   turbopack: {
     root: process.cwd(),
   },
--- release.Dockerfile
+++ release.Dockerfile
@@ -3,8 +3,8 @@ FROM node:20.19.0 AS base
 WORKDIR /app
 EXPOSE 3000
 EXPOSE 8000
-ENV NEXT_PUBLIC_API_BASE_URL "http://18.215.234.20:8080/api"
-ENV NEXT_PUBLIC_WS_URL "http://18.215.234.20:8080/ws/simulacion"
+ENV NEXT_PUBLIC_API_BASE_URL "http://3.91.199.44:8080/api"
+ENV NEXT_PUBLIC_WS_URL "http://3.91.199.44:8080/ws/simulacion"
 
 ADD . .
 #RUN apt-get update && apt-get -y install chromium
